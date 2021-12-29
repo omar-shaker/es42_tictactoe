d205b8e9ae841e03aa3cb232865b73aab431220c
4AKER <omarshaker636@gmail.com>
2021-12-29 18:38:30 +0200

    _V1_ Adding features:     -colors for x, o.     -highlight winning boxes.     -score tracking.
    enhancements for game logic and bug fixes.

--- src/es42_tictactoe/Controller.java
+++ src/es42_tictactoe/Controller.java
@@ -1,6 +1,7 @@
 package es42_tictactoe;
 
 import java.net.URL;
+import javafx.animation.PauseTransition;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
@@ -11,6 +12,7 @@ import javafx.geometry.Pos;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.TilePane;
 import javafx.scene.text.Font;
+import javafx.util.Duration;
 
 public class Controller {
     private Stage stage;
@@ -34,6 +36,7 @@ public class Controller {
         stage.setScene(scene);
         stage.show();
     }//homeScene
+    
     public void singlePlayer(ActionEvent event){
         try{
             URL fxml = this.getClass().getResource("WinScene.fxml");
@@ -47,23 +50,25 @@ public class Controller {
         stage.setScene(scene);
         stage.show();
     }//singlePlayer    
+    
     public void multiPlayer(ActionEvent event){
         TilePane gameTile = new TilePane();//contain the boxes of the game
         gameTile.setPrefColumns(3);
         TextField[][] gameBoxes = new TextField[3][3];//The boxes of the game
-        boolean[] player = new boolean[2];//0 --> player1 && 1 --> player2
-        player[0] = false;//Default begin : player1 still not win yet
-        player[1] = false;//Default begin : player2 still not win yet
-        boolean[] turn = new boolean[1];//0 --> 'O' && 1 --> 'X'
-        turn[0] = true; // To begin playing with 'X'
+        int[] player = new int[2];//0 --> playerX && 1 --> playerO
+        player[0] = 0;//Default begin : playerX score
+        player[1] = 0;//Default begin : playerO score
+        boolean isWinner[] = new boolean[1];
+        isWinner[0] = false; // defines whether there is a winner or not
+        int[] turn = new int[1]; //0 --> 'O' && 1 --> 'X'
+        turn[0] = 1; // To begin playing with 'X'
         int[] cord = new int[2];//The "X,Y" Coordinates ... 0 --> 'Y' && 1 --> 'X'
         cord[0] = 0;//set default 'Y' value
         cord[1] = 0;//set default 'X' value
         try{
             URL fxml = this.getClass().getResource("WinScene.fxml");
             winRoot = FXMLLoader.load(fxml);
-        }//try
-        catch(Exception e){
+        } catch(Exception e){
             System.out.println(e.getMessage());
         }//catch
         for(int i = 0 ; i <3 ; i++){
@@ -75,101 +80,107 @@ public class Controller {
                 gameBox.setAlignment(Pos.CENTER);
                 gameBox.setPrefSize(100, 100);
                 gameBox.setOnKeyPressed((e) ->{
-                switch(e.getCode()){
-                    case RIGHT:
-                        if( !( player[0] || player[1] ) ){
-                            cord[1]++;
-                            if(cord[1] == 3) cord[1] = 0;
-                            gameBoxes[cord[0]][cord[1]].requestFocus();
-                        }//if
-                        break;
-                    case LEFT:
-                        if( !( player[0] || player[1] ) ){
-                            cord[1]--;
-                            if(cord[1] < 0) cord[1] = 2;
-                            gameBoxes[cord[0]][cord[1]].requestFocus();
-                        }//if
-                        break;
-                    case DOWN:
-                        if( !( player[0] || player[1] ) ){
-                            cord[0]++;
-                            if(cord[0] == 3) cord[0] = 0;
-                            gameBoxes[cord[0]][cord[1]].requestFocus();
-                        }//if
-                        break;
-                    case UP:
-                        if( !( player[0] || player[1] ) ){
-                            cord[0]--;
-                            if(cord[0] < 0) cord[0] = 2;
-                            gameBoxes[cord[0]][cord[1]].requestFocus();
-                        }//if
-                        break;
-                    case SPACE://put 'O' .... turn[0] = false at 'O' turn
-                        if( player[0] || player[1] || turn[0]){
-                            break;
-                        }//if
-                        if(gameBoxes[cord[0]][cord[1]].getText().equals("")){
-                            gameBoxes[cord[0]][cord[1]].setText("o");
-                            if(gameBoxes[cord[0]][0].getText().equals(gameBoxes[cord[0]][1].getText()) && gameBoxes[cord[0]][0].getText().equals(gameBoxes[cord[0]][2].getText())){
-                                player[0] = true;//Player0 won
-                                scene = new Scene(winRoot);
-                                stage.setScene(scene);
-                            }//if 
-                            else if(gameBoxes[0][cord[1]].getText().equals(gameBoxes[1][cord[1]].getText()) && gameBoxes[0][cord[1]].getText().equals(gameBoxes[2][cord[1]].getText())){
-                                player[0] = true;//Player0 Won
-                                scene = new Scene(winRoot);
-                                stage.setScene(scene);                         
-                            }//else if
-                            else if(cord[0] == cord[1]){
-                                if(gameBoxes[0][0].getText().equals(gameBoxes[1][1].getText()) && gameBoxes[0][0].getText().equals(gameBoxes[2][2].getText())){
-                                    player[0] = true;//Player0 Won
-                                    scene = new Scene(winRoot);
-                                    stage.setScene(scene);
-                                }//if
-                            }//else if
-                            else if((cord[0] + cord[1]) == 2){
-                                if(gameBoxes[0][2].getText().equals(gameBoxes[1][1].getText()) && gameBoxes[0][2].getText().equals(gameBoxes[2][0].getText())){
-                                    player[0] = true;//Player0 Won
-                                    scene = new Scene(winRoot);
-                                    stage.setScene(scene);
-                                }//if
-                            }//else if
-                            turn[0] = true;//To make the turn for 'X' player
-                        }//if
-                        break;
-                    case ENTER:
-                        if( player[0] || player[1] || (!turn[0])){ 
-                            break;
-                        }//if
-                        if(gameBoxes[cord[0]][cord[1]].getText().equals("")){
-                            gameBoxes[cord[0]][cord[1]].setText("x");
-                            if(gameBoxes[cord[0]][0].getText().equals(gameBoxes[cord[0]][1].getText()) && gameBoxes[cord[0]][0].getText().equals(gameBoxes[cord[0]][2].getText())){
-                                player[1] = true;//Player1 Won
-                                scene = new Scene(winRoot);
-                                stage.setScene(scene);
+                    switch(e.getCode()){
+                        case RIGHT:
+                            if( !isWinner[0] ){
+                                cord[1]++;
+                                if(cord[1] == 3) cord[1] = 0;
+                                gameBoxes[cord[0]][cord[1]].requestFocus();
+                                gameBoxes[cord[0]][cord[1]].deselect();
                             }//if
-                            else if(gameBoxes[0][cord[1]].getText().equals(gameBoxes[1][cord[1]].getText()) && gameBoxes[0][cord[1]].getText().equals(gameBoxes[2][cord[1]].getText())){
-                                player[1] = true;//Player1 Won
-                                scene = new Scene(winRoot);
-                                stage.setScene(scene);
-                            }//else if
-                            else if(cord[0] == cord[1]){
-                                if(gameBoxes[0][0].getText().equals(gameBoxes[1][1].getText()) && gameBoxes[0][0].getText().equals(gameBoxes[2][2].getText())){
-                                    player[1] = true;//Player1 Won
+                            break;
+                            
+                        case LEFT:
+                            if( !isWinner[0] ){
+                                cord[1]--;
+                                if(cord[1] < 0) cord[1] = 2;
+                                gameBoxes[cord[0]][cord[1]].requestFocus();
+                                gameBoxes[cord[0]][cord[1]].deselect();
+                            }//if
+                            break;
+
+                        case DOWN:
+                            if( !isWinner[0] ){
+                                cord[0]++;
+                                if(cord[0] == 3) cord[0] = 0;
+                                gameBoxes[cord[0]][cord[1]].requestFocus();
+                                gameBoxes[cord[0]][cord[1]].deselect();
+                            }//if
+                            break;
+
+                        case UP:
+                            if( !isWinner[0] ){
+                                cord[0]--;
+                                if(cord[0] < 0) cord[0] = 2;
+                                gameBoxes[cord[0]][cord[1]].requestFocus();
+                                gameBoxes[cord[0]][cord[1]].deselect();
+                            }//if
+                            break;
+
+                        case SPACE://put 'O' turn[0] = 0, else 'x' 
+                            if( isWinner[0] ){
+                                break;
+                            }//if
+                            if(gameBoxes[cord[0]][cord[1]].getText().equals("")){
+                                switch(turn[0]){
+                                    case 0:
+                                        gameBoxes[cord[0]][cord[1]].setText("o");
+                                        gameBoxes[cord[0]][cord[1]].setStyle("-fx-text-inner-color: blue");
+                                        turn[0] = 1;
+                                        break;
+
+                                    case 1:
+                                        gameBoxes[cord[0]][cord[1]].setText("x");
+                                        gameBoxes[cord[0]][cord[1]].setStyle("-fx-text-inner-color: red");
+                                        turn[0] = 0;
+                                        break;
+                                }
+                                if(gameBoxes[cord[0]][0].getText().equals(gameBoxes[cord[0]][1].getText()) && gameBoxes[cord[0]][0].getText().equals(gameBoxes[cord[0]][2].getText())){
+                                    player[turn[0]]++;//Player won
+                                    gameBoxes[cord[0]][0].setStyle("-fx-background-color: lime");
+                                    gameBoxes[cord[0]][1].setStyle("-fx-background-color: lime");
+                                    gameBoxes[cord[0]][2].setStyle("-fx-background-color: lime");
                                     scene = new Scene(winRoot);
-                                    stage.setScene(scene);
-                                }//if
-                            }//else if
-                            else if((cord[0] + cord[1]) == 2){
-                                if(gameBoxes[0][2].getText().equals(gameBoxes[1][1].getText()) && gameBoxes[0][2].getText().equals(gameBoxes[2][0].getText())){
-                                    player[1] = true;//Player1 Won
+                                    PauseTransition delay = new PauseTransition(Duration.seconds(2));
+                                    delay.setOnFinished( x -> stage.setScene(scene) );
+                                    delay.play();
+                                }//if 
+                                else if(gameBoxes[0][cord[1]].getText().equals(gameBoxes[1][cord[1]].getText()) && gameBoxes[0][cord[1]].getText().equals(gameBoxes[2][cord[1]].getText())){
+                                    player[turn[0]]++;//Player Won
+                                    gameBoxes[0][cord[1]].setStyle("-fx-background-color: lime");
+                                    gameBoxes[1][cord[1]].setStyle("-fx-background-color: lime");
+                                    gameBoxes[2][cord[1]].setStyle("-fx-background-color: lime");
                                     scene = new Scene(winRoot);
-                                    stage.setScene(scene);
-                                }//if
-                            }//else if
-                            turn[0] = false;//To make the turn for 'O' player
-                        }//if
-                        break;
+                                    PauseTransition delay = new PauseTransition(Duration.seconds(2));
+                                    delay.setOnFinished( x -> stage.setScene(scene) );
+                                    delay.play();                        
+                                }//else if
+                                else if(cord[0] == cord[1]){
+                                    if(gameBoxes[0][0].getText().equals(gameBoxes[1][1].getText()) && gameBoxes[0][0].getText().equals(gameBoxes[2][2].getText())){
+                                        player[turn[0]]++;//Player Won
+                                        gameBoxes[0][0].setStyle("-fx-background-color: lime");
+                                        gameBoxes[1][1].setStyle("-fx-background-color: lime");
+                                        gameBoxes[2][2].setStyle("-fx-background-color: lime");
+                                        scene = new Scene(winRoot);
+                                        PauseTransition delay = new PauseTransition(Duration.seconds(2));
+                                        delay.setOnFinished( x -> stage.setScene(scene) );
+                                        delay.play();
+                                    }//if
+                                }//else if
+                                else if((cord[0] + cord[1]) == 2){
+                                    if(gameBoxes[0][2].getText().equals(gameBoxes[1][1].getText()) && gameBoxes[0][2].getText().equals(gameBoxes[2][0].getText())){
+                                        player[turn[0]]++;//Player Won
+                                        gameBoxes[0][2].setStyle("-fx-background-color: lime");
+                                        gameBoxes[1][1].setStyle("-fx-background-color: lime");
+                                        gameBoxes[2][0].setStyle("-fx-background-color: lime");
+                                        scene = new Scene(winRoot);
+                                        PauseTransition delay = new PauseTransition(Duration.seconds(2));
+                                        delay.setOnFinished( x -> stage.setScene(scene) );
+                                        delay.play();
+                                    }//if
+                                }//else if
+                            }//if
+                            break;
                     }//switch
                 });
                 gameTile.getChildren().add(gameBox);
@@ -180,8 +191,10 @@ public class Controller {
         stage.setScene(scene);
         stage.show();
     }//multiPlayer
+    
     public void exit(ActionEvent event){
         stage = (Stage)(((Node)(event.getSource())).getScene().getWindow());
         stage.close();
     }//exit
+    
 }//class Controller
